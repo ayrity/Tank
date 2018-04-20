9d20fbb6474c238f1604ee6d57ede0fe03bd4eac
ayrity <38549467+ayrity@users.noreply.github.com>
2018-04-20 00:29:16 -0400

    Add files via upload

---
+++ RUNandSTOP_speed25.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time
+import atexit
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x6f)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+# set the speed to start, from 0 (off) to 255 (max speed)
+mh.getMotor(3).setSpeed(25)
+mh.getMotor(4).setSpeed(25)
+# turn on motor
+
+
+sensor = 40
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+current = GPIO.input(sensor)
+previous = current
+def printState(current):
+    print 'GPIO pin %s is %s' % (sensor, 'HIGH' if current else 'LOW')
+
+while True:
+    if (0 == GPIO.input(sensor)):
+        print "Forward"
+        mh.getMotor(3).run(Adafruit_MotorHAT.FORWARD)
+        mh.getMotor(4).run(Adafruit_MotorHAT.FORWARD)
+    if (0 != GPIO.input(sensor)):
+        print "Detected Barrier!"
+        mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    time.sleep(1.0)
+GPIO.cleanup()
