65e31c835cfe08e47de707a9f35c854320fa8506
ayrity <38549467+ayrity@users.noreply.github.com>
2018-04-23 17:40:05 -0400

    Add files via upload

---
+++ runbot3.py
@@ -0,0 +1,30 @@
+import time
+import Robot
+
+LEFT_TRIM   = 0
+RIGHT_TRIM  = 0
+
+robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
+
+sensorRIGHT = 40
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensorRIGHT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+sensorLEFT = 38
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensorLEFT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    if (0 == GPIO.input(sensorRIGHT) and 0 == GPIO.input(sensorLEFT)):
+        robot.forward(125, 0)
+    if (0 != GPIO.input(sensorRIGHT)):
+        robot.backward(150, 0.1)
+        robot.left(150, 0.6)
+    if (0 != GPIO.input(sensorLEFT)):
+        robot.backward(150, 0.1)
+        robot.right(150, 0.6)
+    if (0 != GPIO.input(sensorLEFT) and 0 != GPIO.input(sensorRIGHT)):
+        robot.backward(150, 1.0)
+        robot.left(150, 1.4)
+    time.sleep(1.0)
+GPIO.cleanup()
