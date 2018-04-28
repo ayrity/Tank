4b9dfd8d66af27b76b9f2e8c1c00efb70eb07f73
ayrity <38549467+ayrity@users.noreply.github.com>
2018-04-28 16:38:09 -0400

    Add files via upload

---
+++ buttonLEDS.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(35, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
+
+redLED = 37
+greenLED = 35
+button = 31
+
+while True:
+    if button == False:
+        GPIO.output(35, True)
+        GPIO.output(37, False)
+        print('button pressed')
+        time.sleep(0.2)
+    if button == True:
+        GPIO.output(35, False)
+        GPIO.output(37, True)
+        print('ready to start')
+        time.sleep(0.2)
+GPIO.cleanup()
